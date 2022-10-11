a02e6b98aecfd2f59091af06238d754cddc8cde4
chaos023 <chaos@relationshipapp.com>
2022-10-11 11:49:50 +0800

    dabao

---
+++ dist/assets/showButton.3fc8c920.js
@@ -0,0 +1,1 @@
+import{M as t}from"./index.2c638c17.js";const r=t(),u=o=>r.buttons.includes(o);export{u as s};
---
+++ dist/assets/user.aad5f106.js
@@ -0,0 +1,1 @@
+import{N as r}from"./index.2c638c17.js";const s=r("usermodular",{state:()=>({userDetail:""}),persist:{enabled:!0,strategies:[{key:"user",storage:localStorage}]},getters:{achieveuseruserDetail:e=>e.userDetail},actions:{setuserDetail(e){this.userDetail=e},clearuserDetail(){this.userDetail={}}}});export{s as u};
